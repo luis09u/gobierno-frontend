c450b803a00d69c93b6dd34c4bba3f42e976ef0a
luis09u <luisaj020104@gmail.com>
2026-07-08 00:30:27 -0500

    termino dominio apo

--- src/data/procesosCobit.js
+++ src/data/procesosCobit.js
@@ -8,18 +8,18 @@ export const procesosEDM = [
 ];
 
 export const procesosAPO = [
-  { id: 'APO01', nombre: 'Gestionar el marco de gestión de I&T' },
-  { id: 'APO02', nombre: 'Gestionar la estrategia' },
+  { id: 'APO01', nombre: 'Gestionar el marco de gestión de I&T'},
+  { id: 'APO02', nombre: 'Gestionar la estrategia'},
   { id: 'APO03', nombre: 'Gestionar la arquitectura empresarial'},
-  { id: 'APO04', nombre: 'Gestionar la innovación', deshabilitado: true },
-  { id: 'APO05', nombre: 'Gestionar el portafolio', deshabilitado: true },
-  { id: 'APO06', nombre: 'Gestionar el presupuesto y los costos', deshabilitado: true },
-  { id: 'APO07', nombre: 'Gestionar los recursos humanos', deshabilitado: true },
-  { id: 'APO08', nombre: 'Gestionar las relaciones', deshabilitado: true },
-  { id: 'APO09', nombre: 'Gestionar los acuerdos de servicio', deshabilitado: true },
-  { id: 'APO10', nombre: 'Gestionar los proveedores', deshabilitado: true },
-  { id: 'APO11', nombre: 'Gestionar la calidad', deshabilitado: true },
-  { id: 'APO12', nombre: 'Gestionar el riesgo', deshabilitado: true },
-  { id: 'APO13', nombre: 'Gestionar la seguridad', deshabilitado: true },
-  { id: 'APO14', nombre: 'Gestionar los datos', deshabilitado: true }
+  { id: 'APO04', nombre: 'Gestionar la innovación'},
+  { id: 'APO05', nombre: 'Gestionar el portafolio'},
+  { id: 'APO06', nombre: 'Gestionar el presupuesto y los costos'},
+  { id: 'APO07', nombre: 'Gestionar los recursos humanos'},
+  { id: 'APO08', nombre: 'Gestionar las relaciones'},
+  { id: 'APO09', nombre: 'Gestionar los acuerdos de servicio'},
+  { id: 'APO10', nombre: 'Gestionar los proveedores' },
+  { id: 'APO11', nombre: 'Gestionar la calidad' },
+  { id: 'APO12', nombre: 'Gestionar el riesgo' },
+  { id: 'APO13', nombre: 'Gestionar la seguridad'},
+  { id: 'APO14', nombre: 'Gestionar los datos'}
 ];
