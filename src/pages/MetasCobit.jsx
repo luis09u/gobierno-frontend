a62b25813b3a90b1efbef7e0f993f676f25a274f
luis09u <luisaj020104@gmail.com>
2026-07-16 17:18:20 -0500

    relevancia3

--- src/pages/MetasCobit.jsx
+++ src/pages/MetasCobit.jsx
@@ -89,7 +89,7 @@ export default function MetasCobit() {
           </div>
         </div>
 
-        {/* COLUMNA: Metas de Alineamiento (AG) */}
+        {/* COLUMNA: Metas de Alineamiento (AG) ci*/}
         <div>
           <h3 className="text-2xl font-semibold text-emerald-400 mb-6 border-b border-white/10 pb-2 flex items-center gap-3">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
