35c36dee4f20b2fe8d1e7bdb31738aa1559ce444
luis09u <luisaj020104@gmail.com>
2026-07-16 18:40:23 -0500

    relevancia4

--- src/data/procesosCobit.js
+++ src/data/procesosCobit.js
@@ -1,6 +1,6 @@
 export const procesosEDM = [
   { id: 'EDM01', nombre: 'Asegurar el establecimiento y mantenimiento del marco de gobierno' },
-  { id: 'EDM02', nombre: 'Asegurar la entrega de beneficios' },
+  { id: 'EDM02', nombre: 'Asegurar la obtención de beneficios', esCritico: true },
   { id: 'EDM03', nombre: 'Asegurar la optimización del riesgo', esCritico: true },
   { id: 'EDM04', nombre: 'Asegurar la optimización de recursos' },
   { id: 'EDM05', nombre: 'Asegurar la transparencia hacia las partes interesadas' }
@@ -8,7 +8,7 @@ export const procesosEDM = [
 
 export const procesosAPO = [
   { id: 'APO01', nombre: 'Gestionar el marco de gestión de I&T' },
-  { id: 'APO02', nombre: 'Gestionar la estrategia' },
+  { id: 'APO02', nombre: 'Gestionar la estrategia', esCritico: true  },
   { id: 'APO03', nombre: 'Gestionar la arquitectura empresarial' },
   { id: 'APO04', nombre: 'Gestionar la innovación' },
   { id: 'APO05', nombre: 'Gestionar el portafolio' },
@@ -17,23 +17,23 @@ export const procesosAPO = [
   { id: 'APO08', nombre: 'Gestionar las relaciones' },
   { id: 'APO09', nombre: 'Gestionar los acuerdos de servicio' },
   { id: 'APO10', nombre: 'Gestionar los proveedores' },
-  { id: 'APO11', nombre: 'Gestionar la calidad' },
+  { id: 'APO11', nombre: 'Gestionar la calidad', esCritico: true  },
   { id: 'APO12', nombre: 'Gestionar el riesgo', esCritico: true },
-  { id: 'APO13', nombre: 'Gestionar la seguridad' },
+  { id: 'APO13', nombre: 'Gestionar la seguridad', esCritico: true  },
   { id: 'APO14', nombre: 'Gestionar los datos' }
 ];
 
 export const procesosBAI = [
   { id: 'BAI01', nombre: 'Gestionar los programas y proyectos' },
   { id: 'BAI02', nombre: 'Gestionar la definición de requisitos'},
-  { id: 'BAI03', nombre: 'Gestionar la identificación y construcción de soluciones'},
+  { id: 'BAI03', nombre: 'Gestionar la identificación y construcción de soluciones', esCritico: true },
   { id: 'BAI04', nombre: 'Gestionar la disponibilidad y la capacidad'},
   { id: 'BAI05', nombre: 'Gestionar los cambios organizativos'},
   { id: 'BAI06', nombre: 'Gestionar los cambios de TI', esCritico: true },
   { id: 'BAI07', nombre: 'Gestionar la aceptación y la transición de los cambios de TI', esCritico: true },
   { id: 'BAI08', nombre: 'Gestionar el conocimiento'},
   { id: 'BAI09', nombre: 'Gestionar los activos'},
-  { id: 'BAI10', nombre: 'Gestionar la configuración'},
+  { id: 'BAI10', nombre: 'Gestionar la configuración', esCritico: true },
   { id: 'BAI11', nombre: 'Gestionar los proyectos'}
 ];
 
@@ -47,7 +47,7 @@ export const procesosDSS = [
 ];
 
 export const procesosMEA = [
-  { id: 'MEA01', nombre: 'Gestionar la monitorización del rendimiento y la conformidad'},
+  { id: 'MEA01', nombre: 'Gestionar la monitorización del rendimiento y la conformidad', esCritico: true },
   { id: 'MEA02', nombre: 'Gestionar el sistema de control interno'},
   { id: 'MEA03', nombre: 'Gestionar el cumplimiento de los requisitos externos', esCritico: true },
   { id: 'MEA04', nombre: 'Gestionar el aseguramiento'}
